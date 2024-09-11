dbdce1115d04c9c27a022af01297a09086ba76aa
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-11 14:00:17 +0200

    Add text to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,18 @@
 export const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="mx-36 mt-36 place-content-center text-center leading-relaxed">
+      <h1>About me</h1>
+      <p>
+        Jeg er en full-stack utvikler som har fått verdifull erfaring fra salt
+        bootcamp og fra prosjekter i universitet. Bacheloroppgaven hvor vi
+        samarbeidet med en bedrift for å levere en webapplikasjon var et
+        vellykket prosjekt som resulterte i et produkt som bedriften skal bruke
+        internt. Mine ferdigheter ligger i planlegging, design og utvikling av
+        applikasjoner. Personlig har jeg gode kommunikasjonsevner, høy
+        kompetanse innen teknologi og kan jobbe effektivt både selvstendig og i
+        team. Jeg snakker flytende engelsk etter å ha gått på internasjonale
+        skoler i mange år.
+      </p>
+    </div>
+  );
 };
