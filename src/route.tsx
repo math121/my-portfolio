f126b27de17f18194a634f17054a67daf4ad080d
Mathangi Pushparajah <mathangi23@outlook.com>
2024-09-12 17:59:39 +0200

    make text on nav bar bold and underlined on hover

--- src/route.tsx
+++ src/route.tsx
@@ -12,14 +12,26 @@ const rootRoute = createRootRoute({
   component: () => (
     <>
       <nav className="space-x-20 text-center text-xl p-5">
-        <Link to="/" className="no-underline p-4 text-white">
+        <Link
+          to="/"
+          className="no-underline p-4 text-white group relative w-max hover:font-bold"
+        >
           Home
+          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
         </Link>
-        <Link to="/projects" className="no-underline p-4 text-white">
+        <Link
+          to="/projects"
+          className="no-underline p-4 text-white group relative w-max hover:font-bold"
+        >
           Projects
+          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
         </Link>
-        <Link to="/aboutMe" className="no-underline p-4 text-white">
+        <Link
+          to="/aboutMe"
+          className="no-underline p-4 text-white group relative w-max hover:font-bold"
+        >
           About me
+          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
         </Link>
       </nav>
       <Outlet />
