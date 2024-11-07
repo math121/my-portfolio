6873d3ebf72cb1e3c704053d96cf70d057a36279
Mathangi Pushparajah <mathangi23@outlook.com>
2024-11-07 14:30:51 +0100

    remove font style and bg color, remove picture from home page

--- src/route.tsx
+++ src/route.tsx
@@ -14,24 +14,24 @@ const rootRoute = createRootRoute({
       <nav className="space-x-20 text-center text-xl p-5">
         <Link
           to="/"
-          className="no-underline p-4 text-white group relative w-max hover:font-bold"
+          className="no-underline p-4 group relative w-max hover:font-bold"
         >
           Home
-          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
+          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-black group-hover:w-full"></span>
         </Link>
         <Link
           to="/projects"
-          className="no-underline p-4 text-white group relative w-max hover:font-bold"
+          className="no-underline p-4 group relative w-max hover:font-bold"
         >
           Projects
-          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
+          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-black group-hover:w-full"></span>
         </Link>
         <Link
           to="/aboutMe"
-          className="no-underline p-4 text-white group relative w-max hover:font-bold"
+          className="no-underline p-4 group relative w-max hover:font-bold"
         >
           About me
-          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
+          <span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-black group-hover:w-full"></span>
         </Link>
       </nav>
       <Outlet />
