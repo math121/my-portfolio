fabd0d6da39414c1139130be97719a47238be960
Mathangi Pushparajah <mathangi23@outlook.com>
2024-11-07 15:06:27 +0100

    Change to english text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ export const About = () => {
     <div className="mx-36 mt-36 place-content-center text-center leading-relaxed">
       <h1>About me</h1>
       <p>
-        Jeg er en full-stack utvikler som har fått verdifull erfaring fra salt
+        {/*Jeg er en utvikler som har fått verdifull erfaring fra salt
         bootcamp og fra prosjekter i universitet. Bacheloroppgaven hvor vi
         samarbeidet med en bedrift for å levere en webapplikasjon var et
         vellykket prosjekt som resulterte i et produkt som bedriften skal bruke
@@ -11,7 +11,16 @@ export const About = () => {
         applikasjoner. Personlig har jeg gode kommunikasjonsevner, høy
         kompetanse innen teknologi og kan jobbe effektivt både selvstendig og i
         team. Jeg snakker flytende engelsk etter å ha gått på internasjonale
-        skoler i mange år.
+        skoler i mange år.*/}
+        I am a developer who has gained valuable experience from the salt
+        bootcamp and from university projects. The Bachelor’s thesis, where we
+        collaborated with a company to deliver a web application, was a
+        successful project that resulted in a product the company will use
+        internally. My skills are in planning, designing, and developing
+        applications. Personally, I have strong communication skills, a high
+        level of technical competence, and can work effectively both
+        independently and as part of a team. I speak fluent English after
+        attending international schools for many years.
       </p>
     </div>
   );
